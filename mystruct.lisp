2e6d286e799ebe4339a813d066d06644911ffbad
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2019-12-01 11:41:42 +0900

    Add mystruct.lisp

---
+++ mystruct.lisp
@@ -0,0 +1,13 @@
+(defstruct person name email)
+
+(defstruct dog name)
+
+(defun main ()
+  (let ((tom (make-person :name "Tom" :email "tom@tom.com"))
+        (hachi (make-dog :name "Hachi")))
+    (progn
+      (format t "~A~%" (typep tom 'person))
+      (format t "~A~%" (person-name tom))
+      (format t "~A~%" (person-email tom))
+      (format t "~A~%" (typep hachi 'dog))
+      (format t "~A~%" (dog-name hachi)))))
