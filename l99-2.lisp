eab7ef5c65a7a87cc36955ab5072fe65f7737a5d
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2020-06-17 10:28:20 +0900

    Add L99-31 version 2 (Sieve of Eratosthenes??)

--- l99-2.lisp
+++ l99-2.lisp
@@ -3,7 +3,8 @@
 
 (defpackage :l99-2
   (:use :cl)
-  (:export :primep))
+  (:export :primep
+           :primep2))
 (in-package :l99-2)
 
 ;; L-31 Determine whether a given integer number is prime.
@@ -17,3 +18,24 @@
                         (is-not-divisor (+ d 1))))))
       (and (/= nn 1)
            (is-not-divisor 2)))))
+
+; version 2
+(defun filter (n l)
+  (loop for i in l when (/= (mod i n) 0)
+        collect i))
+
+(defun primep2 (n)
+  (let ((l (loop for i from 2 upto n collect i)))
+    (labels ((make-primes (acc ls)
+               (cond
+                 ((endp ls) acc)
+                 (t
+                  (if (< (car ls) (sqrt n))
+                      (make-primes (cons (car ls) acc) (filter (car ls) (cdr ls)))
+                      (append (cons (car ls) acc) (cdr ls)))))))
+      (let ((primes (make-primes '() l)))
+        (unless (< n 2)
+            (if (eql (find n primes) nil)
+                nil
+                t))))))
+            
