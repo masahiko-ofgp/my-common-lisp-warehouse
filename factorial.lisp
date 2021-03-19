d0ff7a339a7936c4c508a21f589b96f2835953b4
masahiko-ofgp <ichigyo.zanmai@gmail.com>
2021-03-19 16:26:55 +0900

    Modified function fact

--- factorial.lisp
+++ factorial.lisp
@@ -4,11 +4,10 @@
   (:export :fact))
 (in-package :factorial)
 
-(defun fact-cps (n cont)
-  (if (zerop n)
-    (funcall cont 1)
-    (fact-cps (1- n) (lambda (x) (funcall cont (* n x))))))
 
-;; factorial (continue passing style)
 (defun fact (n)
-  (fact-cps n #'(lambda (x) x)))
+  (labels ((aux (n f)
+             (if (zerop n)
+               (funcall f 1)
+               (aux (1- n) (lambda (x) (funcall f (* n x)))))))
+    (aux n #'(lambda (x) x))))
